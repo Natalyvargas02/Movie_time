5eacdc81c4f72e2e40c73ba8486618d0e0a1f946
Natalyvargas02 <78391470+Natalyvargas02@users.noreply.github.com>
2023-04-05 19:06:31 -0500

    Mi primera subida

---
+++ src/test/java/com/usta/movie_time/MovieTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usta.movie_time;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieTimeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
